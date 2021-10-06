74cc63a404e97c130f4bce6d8f82fbb523954103
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2021-10-06 23:19:48 +0900

    打算尝试使用paddleOCR挽救一下可怜的圣遗物识别

---
+++ src/plugins/rating/test.py
@@ -0,0 +1,26 @@
+from paddleocr import PaddleOCR, draw_ocr
+
+# Paddleocr目前支持的多语言语种可以通过修改lang参数进行切换
+# 例如`ch`, `en`, `fr`, `german`, `korean`, `japan`
+ocr = PaddleOCR(lang="ch", use_gpu=False)  # need to run only once to download and load model into memory
+img_path = '/Users/mark_chen/Downloads/FFB8390C42403B37B6635EA6CCD660F6.jpg'
+
+result = ocr.ocr(img_path, cls=True)
+
+with open('image_info.txt','w') as f:
+    f.write(str(result))
+
+# for line in result:
+#     print(line)
+
+#  显示结果
+# from PIL import Image
+
+# image = Image.open(img_path).convert('RGB')
+# boxes = [line[0] for line in result]
+# txts = [line[1][0] for line in result]
+# scores = [line[1][1] for line in result]
+# im_show = draw_ocr(image, boxes, txts, scores, font_path='./fonts/simfang.ttf')
+# im_show = Image.fromarray(im_show)
+# im_show.save('result.jpg')
+
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -5,7 +5,12 @@ function isMaster(userID) {
 }
 
 async function sendPrompt(sendID, userID, name, auth, type, bot) {
-  await bot.sendMessage(sendID, `您当前无${auth}权限。牛头人是不好的！`, type, userID);
+  await bot.sendMessage(
+    sendID,
+    `您当前无${auth}权限。牛头人是不好的！`,
+    type,
+    userID
+  );
 }
 
 async function setAuth(auth, target, isOn) {
