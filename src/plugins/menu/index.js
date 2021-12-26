b81e40fab6d94a76c8432c9a77be39c089c9ad3b
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2021-12-26 22:43:48 +0900

    添加2.4兑换码，menu提醒多吃蔬菜

--- src/plugins/menu/index.js
+++ src/plugins/menu/index.js
@@ -25,10 +25,15 @@ async function Plugin(Message, bot) {
   const favFood = isArknightsGroup ? "炭烤沙虫腿" : "派蒙";
   const nickname = isArknightsGroup ? "博士" : "旅行者";
 
+  const breakfastItem = Math.random() < 0.98 ? breakfast[getRandomInt(breakfast.length) - 1] : favFood;
+  const lunchItem = Math.random() < 0.98 ? lunch[getRandomInt(lunch.length) - 1] : favFood;
+  const dinnerItem = Math.random() < 0.98 ? dinner[getRandomInt(dinner.length) - 1] : favFood;
+  const eatMoreVegetable = `${breakfastItem}${lunchItem}${dinnerItem}`.match(/[煎炸炒烤油]/) ? `\n${nickname}记得多吃蔬菜，不要上火哦。` : "";
+
   const message = `为${nickname}推荐的菜单是：
-早餐：${Math.random() < 0.98 ? breakfast[getRandomInt(breakfast.length) - 1] : favFood}
-午餐：${Math.random() < 0.98 ? lunch[getRandomInt(lunch.length) - 1] : favFood}
-晚餐：${Math.random() < 0.98 ? dinner[getRandomInt(dinner.length) - 1] : favFood}`;
+早餐：${breakfastItem}
+午餐：${lunchItem}
+晚餐：${dinnerItem}${eatMoreVegetable}`;
 
   await bot.sendMessage(sendID, message, type, userID);
   return null;
