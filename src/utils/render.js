e39ea0fee5cbc5dbea6595eab4b3f63646685caf
Mark9804 <fzmwczl@gmail.com>
2021-12-27 20:42:50 +0900

    Raspi: Do not use broken chromium from puppeteer

--- src/utils/render.js
+++ src/utils/render.js
@@ -28,6 +28,7 @@ let browser;
 async function launch() {
   if (undefined === browser) {
     browser = await puppeteer.launch({
+      executablePath: 'chromium-browser',
       defaultViewport: null,
       headless: 0 === global.config.viewDebug,
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
