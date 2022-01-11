cfdb5203971451a4eaa62e69676a92351ea83092
Mark9804 <fzmwczl@gmail.com>
2022-01-11 16:59:36 +0900

    prophecy 屏蔽幸运 level 输出

--- src/plugins/tools/prophecy.js
+++ src/plugins/tools/prophecy.js
@@ -2,7 +2,8 @@ import { getRandomInt } from "../../utils/tools.js";
 
 async function prophecy(msg) {
   const data = global.prophecy.data[getRandomInt(global.prophecy.data.length) - 1];
-  const message = `${data.lucky}\n${data.summary}：${data.text}。\n${data.annotation}`;
+  const message = `${data.text}。\n${data.annotation}`;
+  // const message = `${data.lucky}\n${data.summary}：${data.text}。\n${data.annotation}`;
   msg.bot.say(msg.sid, message, msg.type, msg.uid, true, "\n");
 }
 
